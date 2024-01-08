c752e65403bbdab20d0a0a969271682304eda57f
itsuki <larc.ituki@gmail.com>
2024-01-08 11:27:30 +0900

    [feature] UserModelにemailのフィールドを追加、登録処理の実装

--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -1,9 +1,15 @@
-import { Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
 import { UserService } from './user.service';
-import { User } from './model/user.model';
+import { User as UserModel } from './model/user.model';
+import { CreateUserInput } from './dto/create.user.input';
+import { User } from '@prisma/client';
 
 @Resolver()
 export class UserResolver {
   constructor(private readonly userService: UserService) {}
-  
+
+  @Mutation(() => UserModel)
+  async createUser(@Args('createUserInput') createUserInput: CreateUserInput) :Promise<User> {
+    return await this.userService.createUser(createUserInput);
+  }
 }
